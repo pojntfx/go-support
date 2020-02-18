80f191e8a3b83cfba27e23448f97a7006aa66425
Felix Pojtinger <felix@pojtinger.com>
2020-02-18 21:45:25 +0100

    feat: Add inital structure

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "main.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
